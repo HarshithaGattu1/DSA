c8f6fe208ff5e1818e799915af57e6a56a08fadf
Harshitha <harshinani9441@gmail.com>
2026-02-15 23:07:15 +0530

    Time: 2 ms (99.97%) | Memory: 46.8 MB (89.32%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.java
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.java
@@ -1,15 +1,27 @@
 class Solution {
     public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<bulbs.size();i++) {
-            if(!(list.contains(bulbs.get(i)))) {
-                list.add(bulbs.get(i));
-            }
-            else {
-                list.remove(bulbs.get(i));
-            }
+        //O(n*n);
+        // List<Integer> list = new ArrayList<>();
+        // for(int i=0;i<bulbs.size();i++) {
+        //     if(!(list.contains(bulbs.get(i)))) {
+        //         list.add(bulbs.get(i));
+        //     }
+        //     else {
+        //         list.remove(bulbs.get(i));
+        //     }
+        // }
+        // Collections.sort(list);
+        // return list;
+
+        //O(n)
+        boolean[] bulbStatus = new boolean[101];
+        for(int bulb : bulbs) {
+            bulbStatus[bulb] = !bulbStatus[bulb];
         }
-        Collections.sort(list);
+        List<Integer> list = new ArrayList<>();
+        for(int i=1;i<101;i++) {
+            if(bulbStatus[i]) list.add(i);
+        }
         return list;
     }
 }
